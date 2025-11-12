b79317cff8d2a3253a7bc7f1ddc8bf9afe9c0a43
He Sun <sh.sun@mail.utoronto.ca>
2025-11-11 23:31:33 -0500

    Added classes for use case remove movie

---
+++ src/main/java/use_case/remove_movie/RemoveMovieRoomDataAccessInterface.java
@@ -0,0 +1,10 @@
+package use_case.remove_movie;
+
+import java.util.List;
+
+public interface RemoveMovieRoomDataAccessInterface {
+    boolean isHost();
+    boolean isLocked();
+    boolean removeMovie(String movieID);
+    List<String> getMovieIDs();
+}
