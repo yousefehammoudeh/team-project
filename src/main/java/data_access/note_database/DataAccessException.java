8613f33e81a218833dc9fa4a9f7407833bcee832
He Sun <sh.sun@mail.utoronto.ca>
2025-11-18 01:13:04 -0500

    Added NoteDatabase for direct interaction with the note API

---
+++ src/main/java/data_access/note_database/DataAccessException.java
@@ -0,0 +1,7 @@
+package data_access.note_database;
+
+public class DataAccessException extends Exception {
+    public DataAccessException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/data_access/note_database/NoteDataAccessObject.java
@@ -0,0 +1,137 @@
+package data_access.note_database;
+
+import okhttp3.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class NoteDataAccessObject implements NoteDatabase {
+    private static final int SUCCESS_CODE = 200;
+    private static final int CREDENTIAL_ERROR = 401;
+    private static final int NOT_FOUND_ERROR = 404;
+    private static final int CONFLICT_ERROR = 409;
+    private static final String API_URL = "http://vm003.teach.cs.toronto.edu:20112";
+    private static final String CONTENT_TYPE_LABEL = "Content-Type";
+    private static final String CONTENT_TYPE_JSON = "application/json";
+    private static final String STATUS_CODE_LABEL = "status_code";
+    private static final String USER = "user";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String MESSAGE = "message";
+    private static final String NOTE = "note";
+    private static final String TOKEN = "token";
+    private static final String INFO = "info";
+
+    public String register(String username) throws DataAccessException {
+        final OkHttpClient client = new OkHttpClient().newBuilder().build();
+        final Request request = new Request.Builder()
+                .url(String.format("%s/signUp?username=%s", API_URL, username))
+                .addHeader(CONTENT_TYPE_LABEL, CONTENT_TYPE_JSON)
+                .build();
+        try {
+            final Response response = client.newCall(request).execute();
+
+            final JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE) {
+                return responseBody.getString(TOKEN);
+            }
+            else if (responseBody.getInt(STATUS_CODE_LABEL) == CONFLICT_ERROR) {
+                throw new DataAccessException("User already exists.");
+            }
+            else {
+                throw new DataAccessException("Database error: " + responseBody.getString(MESSAGE));
+            }
+        }
+        catch (IOException | JSONException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+
+    public String getPassword(String username) throws DataAccessException {
+        final OkHttpClient client = new OkHttpClient().newBuilder().build();
+        final Request request = new Request.Builder()
+                .url(String.format("%s/user?username=%s", API_URL, username))
+                .addHeader(CONTENT_TYPE_LABEL, CONTENT_TYPE_JSON)
+                .build();
+        try {
+            final Response response = client.newCall(request).execute();
+
+            final JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE) {
+                return responseBody.getJSONObject(USER).getString(TOKEN);
+            }
+            else if (responseBody.getInt(STATUS_CODE_LABEL) == NOT_FOUND_ERROR) {
+                throw new DataAccessException("User not found.");
+            }
+            else {
+                throw new DataAccessException("Database error: " + responseBody.getString(MESSAGE));
+            }
+        }
+        catch (IOException | JSONException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+
+    public String saveNote(String username, String password, String note) throws DataAccessException {
+        final OkHttpClient client = new OkHttpClient().newBuilder().build();
+        final MediaType mediaType = MediaType.parse(CONTENT_TYPE_JSON);
+        final JSONObject requestBody = new JSONObject();
+        requestBody.put(USERNAME, username);
+        requestBody.put(PASSWORD, password);
+        final JSONObject extra = new JSONObject();
+        extra.put(NOTE, note);
+        requestBody.put(INFO, extra);
+        final RequestBody body = RequestBody.create(requestBody.toString(), mediaType);
+        final Request request = new Request.Builder()
+                .url(String.format("%s/modifyUserInfo", API_URL))
+                .method("PUT", body)
+                .addHeader(CONTENT_TYPE_LABEL, CONTENT_TYPE_JSON)
+                .build();
+        try {
+            final Response response = client.newCall(request).execute();
+
+            final JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE) {
+                return loadNote(username);
+            }
+            else if (responseBody.getInt(STATUS_CODE_LABEL) == CREDENTIAL_ERROR) {
+                throw new DataAccessException("message could not be found or password was incorrect");
+            }
+            else {
+                throw new DataAccessException("database error: " + responseBody.getString(MESSAGE));
+            }
+        }
+        catch (IOException | JSONException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+
+    public String loadNote(String username) throws DataAccessException {
+        final OkHttpClient client = new OkHttpClient().newBuilder().build();
+        final Request request = new Request.Builder()
+                .url(String.format("%s/user?username=%s", API_URL, username))
+                .addHeader(CONTENT_TYPE_LABEL, CONTENT_TYPE_JSON)
+                .build();
+        try {
+            final Response response = client.newCall(request).execute();
+
+            final JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE) {
+                final JSONObject userJSONObject = responseBody.getJSONObject(USER);
+                final JSONObject data = userJSONObject.getJSONObject(INFO);
+                return data.getString(NOTE);
+            }
+            else {
+                throw new DataAccessException(responseBody.getString(MESSAGE));
+            }
+        }
+        catch (IOException | JSONException ex) {
+            throw new DataAccessException(ex.getMessage());
+        }
+    }
+}
