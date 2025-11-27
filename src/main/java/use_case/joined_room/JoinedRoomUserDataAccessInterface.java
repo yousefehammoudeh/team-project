3ea8ef5f4d57c5e62aabeb9faf937d4eecfbd569
elainexing <elainexing99@gmail.com>
2025-11-27 14:25:09 -0500

    S02 use case - fixed the interactor to match room database

--- src/main/java/use_case/joined_room/JoinedRoomUserDataAccessInterface.java
+++ src/main/java/use_case/joined_room/JoinedRoomUserDataAccessInterface.java
@@ -1,6 +1,8 @@
 
 package use_case.joined_room;
 
+import data_access.note_database.DataAccessException;
+
 public interface JoinedRoomUserDataAccessInterface {
 
     void leaveRoom(String roomCode);
